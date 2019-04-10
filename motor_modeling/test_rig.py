69b7de26e6ce8cfefce4ffe714cfc523c7577f24
Jarrod <jjrisley@bu.edu>
2019-04-10 12:00:41 -0400

    Added tinyhawk_data_capture.py for automated testing and data logging for the Tinyhawk.

---
+++ motor_modeling/test_rig.py
@@ -0,0 +1,193 @@
+import serial
+import csv
+import sys
+
+class test_rig:
+
+    #
+    # Global Class Variables
+    #
+    CAL_THRUST_CONST = 1
+    CAL_LEFT_CONST = 1
+    CAL_RIGHT_CONST = 1
+
+    NUM_SAMPLES = 100 # Change this to change the number of samples to take when calibrating
+
+    thrust = 0
+    leftTorque = 0
+    rightTorque = 0
+
+
+    #
+    # Constructor
+    #
+    def __init__(self, dstAddress):
+        self.ser = serial.Serial(dstAddress)
+        self.ser.flushInput()
+
+        self.thrust = 0
+        self.leftTorque = 0
+        self.rightTorque = 0
+        self.rpm = 0
+
+    #
+    # Methods
+    #
+    def read(self):
+
+        try:
+            serialLine = self.ser.readline()
+            decodedLine = serialLine.decode("utf-8").split()
+            #print("decodedLine ", decodedLine)
+
+            if len(decodedLine) == 5:
+
+                self.thrust = (float(decodedLine[0]) + 41.7) / 89300
+
+                self.leftTorque = (float(decodedLine[1]) - 6340) / 2560000
+
+                self.rightTorque = (float(decodedLine[2]) - 3580) / 2860000
+
+                self.rpm = float(decodedLine[4])
+
+            return [self.thrust, self.leftTorque, self.rightTorque, self.rpm]
+
+        except Exception as e:
+            print(e)
+
+"""
+
+if __name__ == "__main__":
+
+    rig = test_rig('COM1')
+    rig.read() # prime the system
+
+    if (sys.argv[1] == 'torque'): # Calibrate Torque
+        print("----- Calibrating Torque -----")
+
+        leverLength = 0.1305 # 130.5 mm
+        weight = 0
+        samples = [0] * 100
+
+        # ----- Right Load Cell Calibration ------ #
+
+#        print("Activating Right Load Cell")
+#        for z in range(5):
+#            weight += float(input("Input the mass (g) you are currently ADDING. Press [Enter] when ready."))
+#
+#            print("Collecting data...", end='')
+#            s = 0
+#            rig.ser.reset_input_buffer()
+#            while s < rig.NUM_SAMPLES:
+#                samples[s] = float(rig.read()[2])
+#                s += 1
+#                print("Sample #", s)
+#
+#
+#            with open("right.csv", "a") as f:
+#                writer = csv.writer(f)
+#                writer.writerow([leverLength * (9.8 * weight / 1000), sum(samples)/len(samples)]) # Should be <Torque, load cell reading>
+#
+#            print("Done.")
+#
+#        for z in range(5):
+#            weight -= float(input("Input the mass (g) you are currently REMOVING. Press [Enter] when ready."))
+#
+#            print("Collecting data", end='')
+#            s = 0
+#            rig.ser.reset_input_buffer()
+#            while s < rig.NUM_SAMPLES:
+#                samples[s] = float(rig.read()[2])
+#                s += 1
+#                print("Sample #", s)
+#
+#
+#            with open("right.csv", "a") as f:
+#                writer = csv.writer(f)
+#                writer.writerow([leverLength * (9.8 * weight / 1000), sum(samples)/len(samples)]) # Should be <Torque, load cell reading>
+#
+#            print("Done.")
+
+        # ----- Left Load Cell Calibration ------ #
+
+        print("Activating Left Load Cell")
+        for z in range(5):
+            weight += float(input("Input the mass you are currently ADDING. Press [Enter] when ready."))
+
+            print("Collecting data...", end='')
+            s = 0
+            rig.ser.reset_input_buffer()
+            while s < rig.NUM_SAMPLES:
+                samples[s] = float(rig.read()[1])
+                s += 1
+                print("Sample #", s)
+
+
+            with open("left.csv", "a") as f:
+                writer = csv.writer(f)
+                writer.writerow([leverLength * (9.8 * weight / 1000), sum(samples)/len(samples)])
+
+            print("Done.")
+
+        for z in range(5):
+            weight -= float(input("Input the mass you are currently REMOVING. Press [Enter] when ready."))
+
+            print("Collecting data", end='')
+            s = 0
+            rig.ser.reset_input_buffer()
+            while s < rig.NUM_SAMPLES:
+                samples[s] = float(rig.read()[1])
+                s += 1
+                print("Sample #", s)
+
+
+            with open("left.csv", "a") as f:
+                writer = csv.writer(f)
+                writer.writerow([leverLength * (9.8 * weight / 1000), sum(samples)/len(samples)])
+
+            print("Done.")
+
+    elif (sys.argv[1] == 'thrust'):
+
+        weight = 0
+        samples = [0] * 100
+
+        print("Activating Thrust Load Cell")
+        for z in range(5):
+            weight += float(input("Input the mass you are currently ADDING. Press [Enter] when ready."))
+
+            print("Collecting data...", end='')
+            s = 0
+            rig.ser.reset_input_buffer()
+            while s < rig.NUM_SAMPLES:
+                samples[s] = float(rig.read()[0])
+                s += 1
+                print("Sample #", s)
+
+
+            with open("thrust.csv", "a") as f:
+                writer = csv.writer(f)
+                writer.writerow([9.8 * weight / 1000, sum(samples)/len(samples)]) # <Force, load cell reading>
+
+            print("Done.")
+
+        for z in range(5):
+            weight -= float(input("Input the mass you are currently REMOVING. Press [Enter] when ready."))
+
+            print("Collecting data", end='')
+            s = 0
+            rig.ser.reset_input_buffer()
+            while s < rig.NUM_SAMPLES:
+                samples[s] = float(rig.read()[0])
+                s += 1
+                print("Sample #", s)
+
+
+            with open("thrust.csv", "a") as f:
+                writer = csv.writer(f)
+                writer.writerow([9.8 * weight / 1000, sum(samples)/len(samples)])  # <Force, load cell reading>
+
+            print("Done.")
+
+"""
+        
--- motor_modeling/tinyhawk_data_capture.py
+++ motor_modeling/tinyhawk_data_capture.py
@@ -1,18 +1,28 @@
 from record_data import MSP
 from msp_protocol import *
 import argparse
+import threading
+from test_rig import test_rig
+import time
+import csv
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser("Capture motor data")
-    parser.add_argument('--port', help="", default="/dev/ttyACM0")
-
-    args = parser.parse_args()
+# Store all sensor data here.
+# Format:
+# <Timestamp> <Thrust> <Left Torque> <Right Torque> <RPM>
+sensor_data = []
+go = threading.Event()
+ 
+def send_motor(port, duration):
     board = None
     try:
-        board = MSP(args.port)
+        board = MSP(port)
         board.connect()
 
-        board.ramp(0, 1000, 2000, 1)
+        go.set() # Activate the event flag to start data collection on time
+
+        board.ramp(1, 1000, 2000, duration/2)
+        time.sleep(1)
+        board.ramp(1, 2000, 1000, duration/2)
     except Exception as e:
         print ("Exception ", e)
         if board:
@@ -21,3 +31,46 @@ if __name__ == "__main__":
             board.sendCMD(16, MSP_SET_MOTOR, cmd)
             board.close()
 
+def get_data(port, duration):
+
+    test = test_rig(port)
+
+    go.wait() # Wait until the Tinyhawk wakes up
+
+    start_time = time.time()
+    while (time.time() - start_time <= duration + 3):
+        read_line = test.read()
+
+        if (read_line != None): 
+            read_line.insert(0, time.time() - start_time)
+
+        sensor_data.append(read_line)
+        print("Sensors: ", read_line)
+    
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("Capture motor data")
+    parser.add_argument('--port-send', help="", default="/dev/ttyACM0")
+    parser.add_argument('--port-receive', help="", default="/dev/ttyACM0")
+    parser.add_argument('--duration', help="", default=1, type=float)
+    parser.add_argument('--filename', help="", default="data.csv")
+
+    args = parser.parse_args()
+
+    get_data_thread = threading.Thread(target=get_data, args = (args.port_receive, args.duration))
+    get_data_thread.start()
+
+    send_motor_thread = threading.Thread(target=send_motor, args = (args.port_send, args.duration))
+    send_motor_thread.start()
+
+    send_motor_thread.join()
+    get_data_thread.join()
+
+    #have each thread save into memory a global array all data collecting add a timetamp merge in that way
+    
+    with open(args.filename, "a") as f:
+        writer = csv.writer(f)
+        for z in sensor_data:
+            if (z != None):
+                writer.writerow(z)
